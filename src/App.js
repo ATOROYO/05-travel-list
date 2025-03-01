757ff7e3711351498cb1c20199c6274a01fd8bca
ATOROYO <davidatoroyosika@gmail.com>
2025-03-01 23:43:02 +0300

    Enhance handleSubmit function to validate description and create new item object

--- src/App.js
+++ src/App.js
@@ -27,7 +27,19 @@ function Form() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e);
+
+    if (!description) return;
+
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id: Date.now(),
+    };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
