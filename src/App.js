501cd0f67ca9b40da8b11fef6ec0ff139eab5f87
ATOROYO <davidatoroyosika@gmail.com>
2025-03-01 21:56:06 +0300

    Update initialItems array and implement PackingList and Item components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
 ];
 
 export default function App() {
@@ -27,7 +28,27 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span>
+        <input type="checkbox" checked={item.packed} />
+        {item.quantity} {item.description}
+      </span>
+      <button className="remove">❌</button>
+    </li>
+  );
 }
 
 function Stats() {
