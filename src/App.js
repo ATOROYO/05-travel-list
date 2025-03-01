5bd4272ac0baa266bb93754feaa1c537884d8767
ATOROYO <davidatoroyosika@gmail.com>
2025-03-01 22:03:29 +0300

    Add quantity selection dropdown to the add-form

--- src/App.js
+++ src/App.js
@@ -23,6 +23,12 @@ function Form() {
   return (
     <form className="add-form">
       <h3>What do you need for your 😍 trip?</h3>
+      <select>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+      </select>
     </form>
   );
 }
